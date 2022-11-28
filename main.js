938d56d8e0a87adb25610a4b81711bf2932a887f
Jenny Weijland <jennyweij@outlook.com>
2022-11-28 08:54:44 +0100

    skapat funktioner för mail och bilderna

--- main.js
+++ main.js
@@ -2,16 +2,35 @@
 // 2. 
 // 3. 
 // 4. 
+window.addEventListener("DOMContentLoaded");
 
-/**Öppnar GitHub i en annan flik */
-function openGitHubNewWindow () {
+/**Funktion som öppnar min sida på GitHub */
+function openMyGitHub () {
     const openGitHub = document.getElementById("gitHubLink");
     openGitHub.onclick = window.open("https://github.com/JennyWeij");
     
 }
 
-/**Öppnar LinkedIn i en annan flik */
-function openLinkedInNewWindow () {
+/**Funktion som öppnar min sida på LinkedIn  */
+function openMyLinkedIn () {
     const openLinkedIn = document.getElementById("linkedInLink");
     openLinkedIn.onclick = window.open("https://www.linkedin.com/in/jenny-weijland-frontend/");
+}
+
+/**Funktion som öppnar min mailadress och tillåter användaren att skriva mail till mig */
+function openMyMail () {
+    const openMail = document.getElementById("mailLink");
+    openMail.onclick = window.open("mailto:jennyweij@outlook.com");
+}
+
+/**Funktion som skapar och fadear in en bild som visar mina jobberfarenheter */
+function openMyExperienceEducation () {
+const img = document.createElement("educationPicture");
+
+}
+
+/**Funktion som skapar och fadear in en bild som visar mina utbildningar */
+function openMyExperienceExperience () {
+const img = document.createElement("experiencePicture");
+
 }
