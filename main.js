49155e6ed2da3b167f501f3db25ed271208a6c1e
Jenny Weijland <jennyweij@outlook.com>
2022-11-28 08:44:24 +0100

    lagt till JS för länkarna

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 // 2. 
 // 3. 
 // 4. 
+
+/**Öppnar GitHub i en annan flik */
+function openGitHubNewWindow () {
+    const openGitHub = document.getElementById("gitHubLink");
+    openGitHub.onclick = window.open("https://github.com/JennyWeij");
+    
+}
+
+/**Öppnar LinkedIn i en annan flik */
+function openLinkedInNewWindow () {
+    const openLinkedIn = document.getElementById("linkedInLink");
+    openLinkedIn.onclick = window.open("https://www.linkedin.com/in/jenny-weijland-frontend/");
+}
