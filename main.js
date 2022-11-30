7bbf9598a5312061b6fc282fc8cdc020ff042d2e
Jenny Weijland <jennyweij@outlook.com>
2022-11-30 15:47:06 +0100

    rensat bland css och js

--- main.js
+++ main.js
@@ -1,66 +1,48 @@
 window.addEventListener("DOMContentLoaded");
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+// const hiddenElements = document.querySelectorAll(".hidden");
+// hiddenElements.forEach((el) => observer.observe(el));
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }else {
-            entry.target.classList.add("remove");
-        }
-    });
-});
-
-
-
-
-// const faders = document.querySelectorAll(".fade-in");
-
-// const appearOptions = {
-//     threshold: 0.25,
-//     rootMargin: "0px 0px 50px 0px"
-// };
-
-// const appearOnScroll = new IntersectionObserver
-// (function(
-//     entries, 
-//     appearOnScroll
-// ) {
-//     entries.forEach(entry => {
-//         if (!entry.isIntersecting) {
-//             return;
-//         } else {
-//             entry.target.classList.add("appear");
-//             appearOnScroll.unobserve(entry.target);
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add("show");
+//         }else {
+//             entry.target.classList.add("remove");
 //         }
 //     });
-// }, 
-// appearOptions);
-
-// faders.forEach(fader => {
-//     appearOnScroll.observe(fader);
-// })
+// });
 
 
-// const button = document.querySelectorAll(".btn")
 
-// const observer = new IntersectionObserver(entries => {
-//         entries.forEach(entry => {
-//             entry.target.classList.toggle("show", entry.isIntersecting)
-//             if (entry.isIntersecting) observer.unobserve(entry.target)
-//         })
-//     }, 
-//     {
-//         threshold: 1,
-//     }
-// )
 
-// button.forEach(button => {
-//     observer.observe(button)
-// })
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+    threshold: 0.25,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(
+    entries, 
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, 
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
 
 
 /**Funktion som skapar och fadear in en bild som visar mina jobberfarenheter */
