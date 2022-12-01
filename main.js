f32764b3d55350f1733619c7ccfadd72b7c08b5e
Jenny Weijland <jennyweij@outlook.com>
2022-12-01 10:36:34 +0100

    uppdaterat JS och html
    Co-authored-by: David Jensen <Tsourdox@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,52 +1,32 @@
-window.addEventListener("DOMContentLoaded");
+window.addEventListener("DOMContentLoaded", main);
 
-// const hiddenElements = document.querySelectorAll(".hidden");
-// hiddenElements.forEach((el) => observer.observe(el));
-
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         console.log(entry)
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add("show");
-//         }else {
-//             entry.target.classList.add("remove");
-//         }
-//     });
-// });
-
-// let options = {
-//     root: document.querySelector('#scrollArea'),
-//     rootMargin: '0px',
-//     threshold: 1.0
-//   }
-
-const faders = document.querySelectorAll(".hidden");
-
-const appearOptions = {
-    root: null,
-    threshold: 0.25,
-    rootMargin: "0px 0px 50px 0px"
-};
-
-const appearOnScroll = new IntersectionObserver
-(function(
-    entries, 
-    appearOnScroll
-) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add(".fade-in");
-            appearOnScroll.unobserve(entry.target);
+function main() {
+    const faders = document.querySelectorAll(".hidden");
+    
+    const appearOptions = {
+        root: null,
+        threshold: 0.5,
+        rootMargin: "0px"
+    };
+    
+    const appearOnScroll = new IntersectionObserver(
+        handleScrollIntersections,
+        appearOptions
+    );
+    
+    function handleScrollIntersections(entries) {
+        console.log('SCROLL');
+        for (const entry of entries) {
+            console.log(entry.target, entry.intersectionRatio)
+            entry.target.style.opacity = entry.intersectionRatio;
         }
-    });
-}, 
-appearOptions);
+    }
+    
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    })
+}
 
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
-})
 
 
 /**Funktion som skapar och fadear in en bild som visar mina jobberfarenheter */
